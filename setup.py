71e2607d878035ed4378f3d3ea9e1247a3b14ca6
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2020-12-16 09:52:56 +0100

    Add github actions

--- setup.py
+++ setup.py
@@ -4,28 +4,44 @@ import setuptools
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 
-README = open(os.path.join(HERE, 'README.md')).read()
-CHANGES = open(os.path.join(HERE, 'CHANGES.md')).read()
+README = open(os.path.join(HERE, "README.md")).read()
+CHANGES = open(os.path.join(HERE, "CHANGES.md")).read()
 
-with open('requirements.txt') as fic:
-    reqs = [a.strip() for a in fic.read().splitlines()
-            if a.strip() and not re.search('^\\s*(#|-e)', a)]
+tests_require = [
+    "factory-boy",
+    "flake8",
+    "coverage",
+    "pylama",
+    "tox",
+    "black",
+    "isort",
+    "tblib",
+    "eradicate",
+    "asynctest",
+]
 
 setuptools.setup(
     name="terra-bonobo-nodes",
-    version=open(os.path.join(HERE, 'terra_bonobo_nodes', 'VERSION.md')).read().strip(),
+    version=open(os.path.join(HERE, "terra_bonobo_nodes", "VERSION.md")).read().strip(),
     include_package_data=True,
     author="Makina Corpus",
     author_email="terralego-pypi@makina-corpus.com",
     description="Set of bonobo ETL's nodes for terralego's projects",
-    long_description=README + '\n\n' + CHANGES,
+    long_description=README + "\n\n" + CHANGES,
     long_description_content_type="text/markdown",
     url="https://github.com/Terralego/terra-bonobo-nodes",
-    install_requires=reqs,
-    dependency_links=[
-        "git+https://github.com/jrmi/pyfiles.git@master#egg=pyfiles",
+    install_requires=[
+        "Django>=2.2,<3.0",
+        "django-geostore",
+        "requests",
+        "bonobo<0.6.9",
+        "elasticsearch>=7.0.0,<8.0.0",
+        "psycopg2",
+        "pytest",
+        "pyfiles @ git+https://github.com/jrmi/pyfiles.git@master",
     ],
-    tests_require=['asynctest'],
+    tests_require=tests_require,
+    extras_require={"dev": tests_require},
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
